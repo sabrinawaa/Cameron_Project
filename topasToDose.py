1ead9578316f8f066e177ce78332a8548011205e
sabrinawaa <sabrinaziqiwang@yahoo.com>
2025-02-04 22:18:59 +0000

    plots scaling works and bins converted to mm

---
+++ topasToDose.py
@@ -0,0 +1,81 @@
+import numpy as np
+from topas2numpy import BinnedResult
+import matplotlib.pyplot as plt
+# simParticles is the number of particles that the simulation was run with
+# acChargenC is the simulated total charge to create an appropriate scaling factor (effectively arbitrary, 10nC is reasonable for now)
+
+def getDosemap(filePath,simParticles,dose_depth,acChargenC=10, plot=False):
+    
+    doseMap = BinnedResult(filePath)#  2D array where each element corresponds to the dose deposited in a specific bin. The dose information is stored in the numerical values of the array elements.
+    rawDosemap=np.squeeze(doseMap.data['Sum'])
+    x,y = doseMap.dimensions[0].get_bin_centers(), doseMap.dimensions[0].get_bin_centers()
+    ratio = (x[1] - x[0])
+    eCharge=1.60217663e-19
+    trueChargenC=(simParticles * eCharge)*1e9
+    scalingFactor=acChargenC/trueChargenC
+    scaledDosemap=np.rot90(rawDosemap * scalingFactor)
+
+    if plot:
+        # plt.figure(figsize=(8, 6))
+        # plt.imshow(scaledDosemap, extent=[0, scaledDosemap.shape[1], 0, scaledDosemap.shape[0]],
+        #            origin='lower', cmap='viridis')
+        # plt.colorbar(label='Dose (Gy)')
+        # plt.title('Dose Distribution at Depth' + str(dose_depth) + 'mm')
+        # plt.xlabel('X Bins')
+        # plt.ylabel('Y Bins')
+        
+        # slice of dose map across axes at origin
+
+        dose_y = scaledDosemap[scaledDosemap.shape[0] // 2, :]  # Middle row (horizontal slice)
+        dose_x = scaledDosemap[:, scaledDosemap.shape[1] // 2]  # Middle column (vertical slice)
+
+        # Create the figure with subplots
+        fig = plt.figure(figsize=(8, 8))
+        grid = plt.GridSpec(4, 4, hspace=0.4, wspace=0.4)  # Create a grid for subplots
+        # Plot the 2D dose map
+        ax_main = fig.add_subplot(grid[1:, :-1])
+        im = ax_main.imshow(scaledDosemap, cmap='viridis', origin='lower')
+        ax_main.set_title("Dose Map at Depth =" + str(dose_depth) + "mm")
+        ax_main.set_xlabel("X (cm)")
+        ax_main.set_ylabel("Y (cm)")
+        cbar = plt.colorbar(im, ax=ax_main, orientation='vertical', fraction=0.046, pad=0.04)
+        cbar.set_label("Dose (Gy)")
+
+        # Plot the histogram along the X-axis
+        ax_x = fig.add_subplot(grid[0, :-1], sharex=ax_main)
+        ax_x.bar(range(dose_x.shape[0]), dose_x, color='blue', alpha=0.7)
+        ax_x.set_ylabel("Sum Dose (Gy)")
+        ax_x.set_title("X Histogram")
+        ax_x.tick_params(axis='x', which='both', bottom=False, labelbottom=False)  # Hide x-ticks
+        ax_x.set_xlim(ax_main.get_xlim()) 
+
+        # Plot the histogram along the Y-axis
+        ax_y = fig.add_subplot(grid[1:, -1], sharey=ax_main)
+        ax_y.barh(range(dose_y.shape[0]), dose_y, color='blue', alpha=0.7)
+        ax_y.set_xlabel("Sum Dose (Gy)")
+        ax_y.set_title("Y Histogram")
+        ax_y.tick_params(axis='y', which='both', left=False, labelleft=False)  # Hide y-ticks
+        ax_y.set_ylim(ax_main.get_ylim())  # Align the Y-axis with the 2D map
+        plt.tight_layout()
+        plt.savefig("Output_figs/dosedepth=" + str(dose_depth) +"dose.png")
+        # Plot X and Y 1D histograms
+
+        
+        # fig1, ax1 = plt.subplots(1, 2, figsize=(10, 5))
+        # # X Projection
+        # ax1[0].bar(range(len(dose_x)), dose_x, width=1.0)
+        # ax1[0].set_title('X Projection of Dose Distribution')   
+        # ax1[0].set_xlabel('X Distribution')   
+        # ax1[0].set_ylabel('Summed Dose (Gy)')   
+
+        # # Y Projection
+        # ax1[1].bar(range(len(dose_y)), dose_y, width=1.0)
+        # ax1[1].set_title('Y Projection of Dose Distribution')   
+        # ax1[1].set_xlabel('Y Distribution')   
+        # ax1[1].set_ylabel('Summed Dose (Gy)')   
+
+        # plt.tight_layout()  # Adjust layout for better spacing
+        # fig1.savefig("Output_figs/dosedepth=" + str(dose_depth) + "_xy_dose.png")
+    return x,y, scaledDosemap
+
+#still need to convert bins to mm 
--- uniformity_fit.py
+++ uniformity_fit.py
@@ -1,17 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
-from scipy.optimize import curve_fit
 from topasToDose import getDosemap
-
 
 #characterise uniformity
 def supergaussian(x, y, A, x0, y0, sigma_x, sigma_y, P):
     return A * np.exp(-( (x-x0)**2 /(2*sigma_x**2) + (y-y0)**2 /(2*sigma_y**2))**P)
 
-def supergaussian1(xy, A, x0, y0, sigma_x, sigma_y, P): #different version of supergaussian for curvefit
-    x,y = xy
-    return A * np.exp(-((x-x0)**2/(2*sigma_x**2) + (y-y0)**2/(2*sigma_y**2))**P)
 
 def MSE(params, x, y, dose_map):
     model = supergaussian(x, y, *params)
@@ -20,33 +15,7 @@ def MSE(params, x, y, dose_map):
 def r90(sig,P):
     return sig * np.sqrt(2) * (-np.log(0.9))**(0.5*P)
 
-def main():
-    n_particles = 100000
-    dose_depth = 500
-    doseMap = getDosemap("DoseAtTank"+str(dose_depth)+".csv",n_particles, dose_depth, plot = False)
-    doseMap = doseMap[10:40, 10:40]  #only the central 60% of the beam
-  
-    x_length, y_length = doseMap.shape
-    x = np.arange(0, x_length, 1)
-    y = np.arange(0, y_length, 1)
-    x, y = np.meshgrid(x, y)
-    gaussian_test = supergaussian(x, y, 1, x_length//2, y_length//2, 8, 8, 2)
-    
-    p0=[0.2, x_length//2, y_length//2, 8, 8, 6]
-    #curvefit 2d histogram to supergaussian
-    # fit_params, cov = curve_fit(supergaussian1, (x.flatten(), y.flatten()), doseMap.flatten(), p0)
-    # Perform the optimization
-    bounds = [(None, None),(None, None), (None, None), (1e-5, None), (1e-5, None), (1, None)] 
-    result = minimize(MSE, p0, args=(x, y, doseMap), method='L-BFGS-B', bounds=bounds)
-    fit_params = result.x
-    print(fit_params)
-
-    # Calculate the fitted super-Gaussian
-    fitted_map = supergaussian(x, y, *fit_params)
-    sig, P = fit_params[3], fit_params[5]
-    r_90 = r90(sig,P)
-    print("r90 = " + str(r_90))
-    
+def plotDoseMap(x, y, doseMap,fitted_map,P,sig,r_90):
     dose_y = fitted_map[fitted_map.shape[0] // 2, :]  # Middle row (horizontal slice)
     dose_x = fitted_map[:, fitted_map.shape[1] // 2]  # Middle column (vertical slice)
 
@@ -54,44 +23,68 @@ def main():
     orig_dose_x = doseMap[:, doseMap.shape[1] // 2]  # Middle column (vertical slice)
 
     # Create the figure with subplots
-    # Create the figure with subplots
     fig = plt.figure(figsize=(10, 8))
-    grid = plt.GridSpec(4, 4, hspace=0.4, wspace=0.4)  # Create a grid for subplots
-    gs = fig.add_gridspec(4, 4)
-
-    # Plot the 2D dose map
-    # ax_main = fig.add_subplot(grid[1:, :-1])
-    ax_main = fig.add_subplot(gs[1:3, 0:2])
-    im = ax_main.imshow(doseMap, extent=[x.min(), x.max(), y.min(), y.max()],
-                        origin='lower', cmap='viridis')
+    grid = plt.GridSpec(4, 5, hspace=0.4, wspace=0.6, width_ratios=[1, 1, 1, 0.1, 1])  
+    print(x.shape,y.shape,doseMap.shape)
+    # Main 2D scatter plot
+    ax_main = fig.add_subplot(grid[1:, :-2])  # Use only the first 3 columns (exclude colorbar space)
+    im = ax_main.scatter(x, y, c=doseMap, cmap="viridis", s=120)
     ax_main.set_title(f"Super-Gaussian  P = {P:.2f}, sigma = {sig:.2f}, r90 = {r_90:.2f}")
-    ax_main.set_xlabel("X Bins")
-    ax_main.set_ylabel("Y Bins")
-    cbar = plt.colorbar(im, ax=ax_main, orientation='vertical', fraction=0.046, pad=0.04)
+    ax_main.set_xlabel("X (mm)")
+    ax_main.set_ylabel("Y (mm)")
+
+    # Colorbar (aligned with the main plot)
+    ax_cbar = fig.add_subplot(grid[1:, -2])
+    cbar = plt.colorbar(im, cax=ax_cbar, orientation='vertical')
     cbar.set_label("Supergaussian Dose (Gy)")
-
-    # Plot the histogram along the X-axis
-    # ax_x = fig.add_subplot(grid[0, :-1], sharex=ax_main)
-    ax_x = fig.add_subplot(gs[0, 0:2])
-    ax_x.bar(range(dose_x.shape[0]), dose_x, alpha=0.4, label="fitted", color="blue", edgecolor="black")
-    ax_x.bar(range(orig_dose_x.shape[0]), orig_dose_x, alpha=0.4, label="orig", color="green", edgecolor="black")
-    ax_x.set_ylabel("Sum Dose (Gy)")
+    print(x[0,:])
+    # Histogram along X-axis (aligned only with the main plot)
+    ax_x = fig.add_subplot(grid[0, :-2], sharex=ax_main)  # Avoid the last two columns
+    # ax_x.bar(x[0,:], dose_x, width=(x[0,1] - x[0,0]), alpha=0.4, label="fitted", color="blue", edgecolor="black")
+    ax_x.plot(x[0,:], dose_x, label="fitted", color="blue" )
+    ax_x.bar(x[0,:], orig_dose_x, width=(x[0,1] - x[0,0]), alpha=0.4, label="orig", color="green", edgecolor="black")
+    ax_x.set_ylabel("Slice Dose (Gy)")
     ax_x.set_title("X Histogram")
-    ax_x.tick_params(axis='x', which='both', bottom=False, labelbottom=False)  # Hide x-ticks
+    ax_x.tick_params(axis='x', which='both', bottom=False, labelbottom=False)
     ax_x.legend()
-
-    # Plot the histogram along the Y-axis
-    # ax_y = fig.add_subplot(grid[1:, -1], sharey=ax_main)
-    ax_y = fig.add_subplot(gs[1:3, 3])
-    ax_y.barh(range(dose_y.shape[0]), dose_y, alpha=0.4, label="fitted", color="blue", edgecolor="black")
-    ax_y.barh(range(orig_dose_y.shape[0]), orig_dose_y, alpha=0.4, label="orig", color="green", edgecolor="black")
-    ax_y.set_xlabel("Sum Dose (Gy)")
+    print(y[:,0])
+    # Histogram along Y-axis
+    ax_y = fig.add_subplot(grid[1:, -1], sharey=ax_main)
+    print(y[1,0] - y[0,0])
+    # ax_y.barh(y[:,0], dose_y, height=(y[1,0] - y[0,0]), alpha=0.4, label="fitted", color="blue", edgecolor="black")
+    ax_y.plot(dose_y, y[:,0], label="fitted", color="blue")
+    ax_y.barh(y[:,0], orig_dose_y, height=(y[1,0] - y[0,0]), alpha=0.4, label="orig", color="green", edgecolor="black")
+    ax_y.set_xlabel("Slice Dose (Gy)")
     ax_y.set_title("Y Histogram")
-    ax_y.tick_params(axis='y', which='both', left=False, labelleft=False)  # Hide y-ticks
+    ax_y.tick_params(axis='y', which='both', left=False, labelleft=False)
     ax_y.legend()
 
     plt.tight_layout()
     plt.show()
+    plt.savefig("Output_figs/SupergaussianFit.png")
+
+    
+def main():
+    n_particles = 100000
+    dose_depth = 500
+    x,y, doseMap = getDosemap("DoseAtTank"+str(dose_depth)+".csv",n_particles, dose_depth, plot = False)
+    x, y = np.meshgrid(x, y)
+    x,y,doseMap = x[10:40, 10:40], y[10:40, 10:40], doseMap[10:40, 10:40]  #only the central 60% of the beam
+    
+    
+    p0=[0.2, np.max(x)//2, np.max(y)//2, 8, 8, 6]
+    #curvefit 2d histogram to supergaussian
+    # Perform the optimization
+    result = minimize(MSE, p0, args=(x, y, doseMap), method='L-BFGS-B')
+    fit_params = result.x
+
+    # Calculate the fitted super-Gaussian
+    fitted_map = supergaussian(x, y, *fit_params)
+    sig, P = fit_params[3], fit_params[5]
+    r_90 = r90(sig,P)
+    plotDoseMap(x, y, doseMap,fitted_map,P,sig,r_90)
+    
+    
 
 main()
         
