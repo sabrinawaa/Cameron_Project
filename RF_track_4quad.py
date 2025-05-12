ff8f466be11a11d93aabb61e86c49cd3ceac603e
sabrinawaa <sabrinaziqiwang@yahoo.com>
2025-05-12 23:01:41 +0200

    updates

--- RF_track_4quad.py
+++ RF_track_4quad.py
@@ -1,18 +1,21 @@
 import RF_Track as RFT
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 RFT.cvar.number_of_threads = 8
 quad_length = 0.3
 k1 = 30
 drift_l = 0.2
 
-def four_quads(Lquad, strength, Ldrift, N_particles, Energy):
+def gaussian(x,A,mu,sig):
+    return A * np.exp(- (x-mu)**2 /(2*sig**2))
 
-    Q1 =RFT.Quadrupole(Lquad, strength/2) 
-    Q2 = RFT.Quadrupole(Lquad, -strength)
-    Q3 = RFT.Quadrupole(Lquad, -strength)
-    Q4 = RFT.Quadrupole(Lquad, -strength)
+def four_quads(Lquad, k11, k12, k13, k14, Ldrift, N_particles, Energy ,plot=True):
+    Q1 =RFT.Quadrupole(Lquad, k11) 
+    Q2 = RFT.Quadrupole(Lquad, k12)
+    Q3 = RFT.Quadrupole(Lquad, k13)
+    Q4 = RFT.Quadrupole(Lquad, k14)
     Drift = RFT.Drift(Ldrift)
     Drift.set_tt_nsteps(50)  
 
@@ -55,8 +58,8 @@ def four_quads(Lquad, strength, Ldrift, N_particles, Energy):
     Twiss.emitt_y = 10
     geo_emm = Twiss.emitt_x / (rel_beta * rel_gamma)
 
-    Twiss.beta_x = 1/geo_emm/1e3 #m
-    Twiss.beta_y = 1/geo_emm/1e3
+    Twiss.beta_x = 1/geo_emm #m to get 1 mm sigma x
+    Twiss.beta_y = 1/geo_emm
     # Twiss.beta_x = 1 * (1 + np.sin(np.radians(mu/2))) / np.sin(np.radians(mu))  # m
     # Twiss.beta_y = 1 * (1 - np.sin(np.radians(mu/2))) / np.sin(np.radians(mu))
     Twiss.alpha_x = 0.0
@@ -69,43 +72,65 @@ def four_quads(Lquad, strength, Ldrift, N_particles, Energy):
     T = lattice.get_transport_table('%S %beta_x %beta_y %alpha_x %alpha_y')
     # print(B1.get_info().beta_x)
     M = B1.get_phase_space('%x %xp %y %yp %E %z')
-    # Make plots
-    plt.figure(1)
-    plt.plot(T[:,0], T[:,1], 'b-', label=r'$\beta_x$')
-    plt.plot(T[:,0], T[:,2], 'r-', label=r'$\beta_y$')
-    plt.plot(T[:,0], T[:,3], 'g-', label=r'$\alpha_x$')
-    plt.plot(T[:,0], T[:,4], 'b-', label=r'$\alpha_y$')
-    plt.legend()
-    plt.xlabel('S [m]')
-    plt.ylabel(r'$\beta$ [m]')
 
-    def scatter_hist(x, y, ax, ax_histx, ax_histy):
+    if plot:
+        # Make plots
+        plt.figure(1)
+        plt.plot(T[:,0], T[:,1], 'b-', label=r'$\beta_x$')
+        plt.plot(T[:,0], T[:,2], 'r-', label=r'$\beta_y$')
+        plt.plot(T[:,0], T[:,3], 'g-', label=r'$\alpha_x$')
+        plt.plot(T[:,0], T[:,4], 'b-', label=r'$\alpha_y$')
+        plt.legend()
+        plt.xlabel('S [m]')
+        plt.ylabel(r'$\beta$ [m]')
 
-        ax_histx.tick_params(axis="x", labelbottom=True)
-        ax_histy.tick_params(axis="y", labelleft=True)
+        def scatter_hist(x, y, ax, ax_histx, ax_histy):
 
-        # the scatter plot:
-        ax.scatter(x, y,s=5)
+            ax_histx.tick_params(axis="x", labelbottom=True)
+            ax_histy.tick_params(axis="y", labelleft=True)
 
-        ax.set_xlabel('X')  # Set x-axis label for scatter plot
-        ax.set_ylabel('Y')
+            # the scatter plot:
+            ax.scatter(x, y,s=5)
 
-        # now determine nice limits by hand:
-        binwidth = 0.25
-        xymax = max(np.max(np.abs(x)), np.max(np.abs(y)))
-        lim = (int(xymax/binwidth) + 1) * binwidth
+            ax.set_xlabel('X (mm)')  # Set x-axis label for scatter plot
+            ax.set_ylabel('Y (mm)')
 
-        bins = np.arange(-lim, lim + binwidth, binwidth)
-        ax_histx.hist(x, bins=bins)
-        ax_histy.hist(y, bins=bins, orientation='horizontal')
+            # now determine nice limits by hand:
+            binwidth = 0.25
+            xymax = max(np.max(np.abs(x)), np.max(np.abs(y)))
+            lim = (int(xymax/binwidth) + 1) * binwidth
 
-    fig, axs = plt.subplot_mosaic([['histx', '.'],
-                                ['scatter', 'histy']],
-                                figsize=(6, 6),
-                                width_ratios=(4, 1), height_ratios=(1, 4),
-                                layout='constrained')
-    scatter_hist(M[:,0], M[:,2], axs['scatter'], axs['histx'], axs['histy'])
-    plt.show()
+            bins = np.arange(-lim, lim + binwidth, binwidth)
+            # ax_histx.hist(x, bins=bins)
+            # ax_histy.hist(y, bins=bins, orientation='horizontal')
+            n_x, bins_x, _ = ax_histx.hist(x, bins=bins, alpha=0.6)
+            n_y, bins_y, _ = ax_histy.hist(y, bins=bins, orientation='horizontal', alpha=0.6)
+
+            mu_x, sigma_x = np.mean(x), np.std(x)
+            mu_y, sigma_y = np.mean(y), np.std(y)
+            
+            # Scale factor to match histogram counts
+            scale_x = len(x) * binwidth
+            scale_y = len(y) * binwidth
+            
+            # Plot Gaussian fits
+            pdf_x = norm.pdf(bins_x, mu_x, sigma_x) * scale_x
+            ax_histx.plot(bins_x, pdf_x, 'k-', linewidth=2, label= f"sig_x = {sigma_x:.2f}")
+            ax_histx.legend()
+            
+            pdf_y = norm.pdf(bins_y, mu_y, sigma_y) * scale_y
+            ax_histy.plot(pdf_y, bins_y, 'k-', linewidth=2, label= f"sig_y = {sigma_y:.2f}")
+            ax_histy.legend()
+
+        fig, axs = plt.subplot_mosaic([['histx', '.'],
+                                    ['scatter', 'histy']],
+                                    figsize=(6, 6),
+                                    width_ratios=(4, 1), height_ratios=(1, 4),
+                                    layout='constrained')
+        scatter_hist(M[:,0], M[:,2], axs['scatter'], axs['histx'], axs['histy'])
+
+        fig.savefig(f"Output_figs/RFT_k1s={k11}_{k12}_{k13}_{k14}.png")
+        np.savetxt(f"RFT_k1s={k11}_{k12}_{k13}_{k14}_N={N_particles}.txt",M) 
     
     return M
 
